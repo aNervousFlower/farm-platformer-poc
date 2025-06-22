6409bbb6dc01bc717e808e32d017df1feed55c5b
Brianne Davis <anervousflower@gmail.com>
2025-06-22 14:06:34 -0600

    fix camera drag on farmer; update collision boxes on boar to match sprite

--- src/Actors/boar.gd
+++ src/Actors/boar.gd
@@ -5,12 +5,6 @@ func _ready() -> void:
 	velocity.x = -speed.x
 
 
-func _on_stomp_detector_body_entered(body: Node2D) -> void:
-	if body.global_position.y > $StompDetector.global_position.y:
-		return
-	queue_free()
-
-
 func _physics_process(delta: float) -> void:
 	velocity.y += gravity * delta
 	if is_on_wall():

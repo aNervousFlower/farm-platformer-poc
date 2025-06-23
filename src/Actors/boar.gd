9c795dd191aeeb409400bf8894492632854c9bcd
Brianne Davis <anervousflower@gmail.com>
2025-06-22 23:56:00 -0600

    implement pacing for boar enemy

--- src/Actors/boar.gd
+++ src/Actors/boar.gd
@@ -1,22 +1,40 @@
 extends Actor
 
 
-func _ready() -> void:
+func _on_visible_on_screen_enabler_2d_screen_entered() -> void:
+	$boar.play("walk")
 	velocity.x = -speed.x
+	$PacingTimer.start()
+
+
+func _on_pacing_timer_timeout() -> void:
+	$boar.play("idle")
+	$PauseTimer.start()
+
+
+func _on_pause_timer_timeout() -> void:
+	velocity.x *= -1
+	$boar.flip_h = velocity.x > 0
+	$boar.play("walk")
+	$PacingTimer.start()
 
 
 func _on_hit_detector_area_entered(area: Area2D) -> void:
 	if area.name == "SwordAttack":
 		is_dead = true
+		$PacingTimer.stop()
+		$PauseTimer.stop()
 		$boar.play("hit")
 		await $boar.animation_finished
 		queue_free()
 
 
 func _physics_process(delta: float) -> void:
-	velocity.y += gravity * delta
 	if is_dead:
 		velocity = Vector2.ZERO
-	if is_on_wall():
-		velocity.x *= -1
-	move_and_slide()
+		
+	velocity.y += gravity * delta
+	#if is_on_wall():
+		#velocity.x *= -1
+	if $PauseTimer.is_stopped():
+		move_and_slide()
